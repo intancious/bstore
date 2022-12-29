06e8ea0c07173bb02e393d1bb36b50859285be87
intancious98 <intancious98@gmail.com>
2022-12-29 20:47:58 +0700

    halo new pos

---
+++ app/Models/Customer.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Models;
+
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;                 // <-- import JWTSubject
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable; // <-- import Auth Laravel
+
+class Customer extends Authenticatable implements JWTSubject // <-- tambahkan "Authenticatable" dan "JWTSubject
+{
+    use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'email', 'email_verified_at', 'password', 'remember_token'
+    ];
+
+    /**
+     * invoice
+     *
+     * @return void
+     */
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
+
+    /**
+     * reviews
+     *
+     * @return void
+     */
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    /**
+     * createdAt
+     *
+     * @return Attribute
+     */
+    // protected function createdAt(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn ($value) => \Carbon\Carbon::locale('id')->parse($value)->translatedFormat('l, d F Y'),
+    //     );
+    // }
+    public function createdAt($data)
+    {
+        $value = Carbon::parse($data);
+        $parse = $value->locale('id');
+        return $parse->translatedFormat('l, d F Y');
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
+
+// public function createdAt($data)
+// {
+//     $value = Carbon::parse($data);
+//     $parse = $value->locale('id');
+//     return $parse->translatedFormat('l, d F Y');
+// }
